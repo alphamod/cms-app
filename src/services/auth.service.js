ef05e3c40e2faf6afab48e067eff9e223c0e5e70
Syed Salman <37833818+alphamod@users.noreply.github.com>
2020-11-16 18:54:55 +0530

    fixed server 1

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:4000/";
+const API_URL = "https://cms-server-alpham.herokuapp.com/";
 
 const register = (username, email, password) => {
     return axios.post(`${API_URL}register`, {
         username, email, password
     });
 };
- 
+
 const login = (username, password) => {
     return axios.post(`${API_URL}login`, {
         username, password
