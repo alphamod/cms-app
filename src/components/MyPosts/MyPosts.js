a2bb7f177d1ea11895ac160052a18fcd22b89a9f
Syed Salman <37833818+alphamod@users.noreply.github.com>
2020-11-16 18:46:58 +0530

    fixed typo

--- src/components/MyPosts/MyPosts.js
+++ src/components/MyPosts/MyPosts.js
@@ -30,20 +30,20 @@ const MyPosts = () => {
                 }
             }
         );
-    },[]);
+    }, []);
 
     return (
         <div className="container-fluid m-2 p-1">
             <h3 className="text-center text-primary font-weight-bolder">My Posts</h3>
             {
-                mypostsxr.reverse().map(post => {
+                myposts.reverse().map(post => {
                     // if(post === null) return (<div className="text-center">No Posts Available</div>);
                     console.log(post);
                     return (
                         <div className="card border-secondary shadow-lg m-2 p-1" key={post._id}>
                             {/* <header className="jumbotron"> */}
                             <div className="card-title py-2 px-4">
-                            <h3 className="">{post.title}</h3>
+                                <h3 className="">{post.title}</h3>
                             </div>
                             {/* </header> */}
                             <div className="card-body text-secondary">
